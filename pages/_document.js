37a2c17e443733af59caae945e2d759c84df6daf
Coin <cce5784@gmail.com>
2022-08-11 03:36:29 +0800

    確認字型選擇
    我們用 Noto Sans TC

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&family=Poppins:wght@300;400&family=Space+Grotesk:wght@400&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;700&family=Poppins:wght@300;400&family=Space+Grotesk:wght@400&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
