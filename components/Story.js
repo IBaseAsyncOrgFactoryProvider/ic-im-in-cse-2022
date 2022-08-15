0c1362aff6e4373bb5efb765bdb139c619bc357b
Coin <cce5784@gmail.com>
2022-08-16 01:42:11 +0800

    換成正式宣傳片

--- components/Story.js
+++ components/Story.js
@@ -5,7 +5,7 @@ export default function Story() {
         <>
             <SectionTitle title="故事背景" />
             <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '3rem' }}>
-                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/vP3rYUNmrgU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/WXND2fYXBZE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </>
   )
