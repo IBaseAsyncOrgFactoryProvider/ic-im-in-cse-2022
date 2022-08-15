e513668c056bef96662b7491151ce90fe318dc4d
Coin <cce5784@gmail.com>
2022-08-16 04:18:45 +0800

    Add GA

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -8,6 +9,19 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;700&family=Poppins:wght@300;400&family=Space+Grotesk:wght@400&display=swap" rel="stylesheet" />
         <meta name="description" content="資想見你——2022 四資迎新" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-L108TS7BNK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-L108TS7BNK');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
