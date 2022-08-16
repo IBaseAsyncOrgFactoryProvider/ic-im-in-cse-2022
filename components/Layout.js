f9f35caf2c89700922dc859adb97d53cd81a43f1
Coin <cce5784@gmail.com>
2022-08-16 09:52:52 +0800

    Add repo link

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,35 @@ import Navbar from './Navbar'
 
 export default function Layout({ children }) {
   return (
-        <>
-            <Navbar />
-            {children}
-            <Footer />
-        </>
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+      <div
+        style={{
+          fontSize: '0.8rem',
+          background: 'var(--color-primary)',
+          color: '#eee',
+          padding: '1rem 0',
+        }}
+      >
+        <div
+          style={{
+            maxWidth: 'var(--large-container-width)',
+            padding: '0 2rem',
+            margin: 'auto',
+          }}
+        >
+          原始碼可在{' '}
+          <a
+            href="https://github.com/IBaseAsyncOrgFactoryProvider/ic-im-in-cse-2022"
+            style={{ textDecoration: 'underline' }}
+          >
+            GitHub
+          </a>{' '}
+          取得。
+        </div>
+      </div>
+    </>
   )
 }
